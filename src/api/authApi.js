2ef05e518c0370834c8aeb7c059ef675276c2540
visionblack3 <136975535+visionblack3@users.noreply.github.com>
2026-09-17 18:44:04 +0530

    Update authApi.js

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,13 +1,13 @@
 import client from "./client";
 
 export const registerUser = (payload) =>
-  client.post("/api/users/registerUser", payload).then((r) => r.data);
+  client.post("/users/registerUser", payload).then((r) => r.data);
 
 export const registerAdmin = (payload) =>
-  client.post("/api/admin/registerAdmin", payload).then((r) => r.data);
+  client.post("/admin/registerAdmin", payload).then((r) => r.data);
 
 export const login = (payload) =>
-  client.post("/api/users/login", payload).then((r) => r.data);
+  client.post("/users/login", payload).then((r) => r.data);
 
 export const getUserById = (id) =>
-  client.get(`/api/users/${id}`).then((r) => r.data);
+  client.get(`/users/${id}`).then((r) => r.data);
